c544c79b5d7a2fd90b2e29e410720910dbe6c7ef
Erik Welch <erik.n.welch@gmail.com>
2022-03-30 23:47:55 -0500

    Setup repo (fingers crossed!)

---
+++ README.md
@@ -0,0 +1,13 @@
+# **GraphBLAS Algorithms**
+
+[![conda-forge](https://img.shields.io/conda/vn/conda-forge/graphblas-algorithms.svg)](https://anaconda.org/conda-forge/graphblas-algorithms)
+[![pypi](https://img.shields.io/pypi/v/graphblas-algorithms.svg)](https://pypi.python.org/pypi/graphblas-algorithms/)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/metagraph-dev/graphblas-algorithms/blob/main/LICENSE)
+[![Tests](https://github.com/metagraph-dev/graphblas-algorithms/workflows/Tests/badge.svg?branch=main)](https://github.com/metagraph-dev/graphblas-algorithms/actions)
+[![Docs](https://readthedocs.org/projects/graphblas-algorithms/badge/?version=latest)](https://graphblas-algorithms.readthedocs.io/en/latest/)
+[![Coverage](https://coveralls.io/repos/metagraph-dev/graphblas-algorithms/badge.svg?branch=main)](https://coveralls.io/r/metagraph-dev/graphblas-algorithms)
+[![Code style](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+GraphBLAS algorithms written in Python with [`grblas`](https://github.com/metagraph-dev/grblas).
+
+This is a work in progress.  Stay tuned!
---
+++ setup.py
@@ -0,0 +1,60 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="graphblas-algorithms",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Graph algorithms written in GraphBLAS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Jim Kitchen and Erik Welch",
+    author_email="erik.n.welch@gmail.com,jim22k@gmail.com",
+    url="https://github.com/metagraph-dev/graphblas-algorithms",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=["grblas"],
+    tests_require=["pytest"],
+    include_package_data=True,
+    license="Apache License 2.0",
+    keywords=[
+        "graphblas",
+        "graph",
+        "sparse",
+        "matrix",
+        "lagraph",
+        "suitesparse",
+        "Networks",
+        "Graph Theory",
+        "Mathematics",
+        "network",
+        "discrete mathematics",
+        "math",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    zip_safe=False,
+)
