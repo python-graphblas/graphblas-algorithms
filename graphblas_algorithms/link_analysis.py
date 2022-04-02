d3e84b9072227b762db4504832ba4ed34debc48b
Erik Welch <erik.n.welch@gmail.com>
2022-04-02 15:24:55 -0500

    Split pagerank into two functions: grblas-native and networkx-facing.
    Also, optimize if adjacency matrix is iso-valued.
    I would bring this implementation to a benchmarking shootout!

--- graphblas_algorithms/link_analysis.py
+++ graphblas_algorithms/link_analysis.py
@@ -1,9 +1,104 @@
 from warnings import warn
 
-import grblas as gb
 import networkx as nx
 import numpy as np
-from grblas.semiring import any_times, plus_first
+from grblas import Matrix, Vector, binary, monoid, unary
+from grblas.semiring import plus_first, plus_times
+
+
+def pagerank_core(
+    A,
+    alpha=0.85,
+    personalization=None,
+    max_iter=100,
+    tol=1e-06,
+    nstart=None,
+    dangling=None,
+    row_degrees=None,
+    name="pagerank",
+):
+    # TODO: consider what happens and what we should do if personalization,
+    # nstart, dangling, and row_degrees input vectors are not dense.
+    N = A.nrows
+    if A.nvals == 0:
+        return Vector.new(float, N, name=name)
+
+    # Initial vector
+    x = Vector.new(float, N, name="x")
+    if nstart is None:
+        x[:] = 1.0 / N
+    else:
+        x << nstart / nstart.reduce()
+
+    # Personalization vector or scalar
+    if personalization is None:
+        p = 1.0 / N
+    else:
+        denom = personalization.reduce().value
+        if denom == 0:
+            raise ZeroDivisionError()
+        p = (personalization / denom).new(name="p")
+
+    # Inverse of row_degrees
+    # Fold alpha constant into S
+    if row_degrees is None:
+        S = A.reduce_rowwise().new(float, name="S")
+        S << alpha / S
+    else:
+        S = (alpha / row_degrees).new(name="S")
+
+    if A.ss.is_iso:
+        # Fold iso-value of A into S
+        # This lets us use the plus_first semiring, which is faster
+        S *= A.reduce_scalar(monoid.any)
+        # S *= A.ss.iso_value  # This would be nice to have
+        semiring = plus_first
+    else:
+        semiring = plus_times
+
+    is_dangling = S.nvals < N
+    if is_dangling:
+        dangling_mask = Vector.new(float, N, name="dangling_mask")
+        dangling_mask(mask=~S.S) << 1.0
+        # Fold alpha constant into dangling_weights (or dangling_mask)
+        if dangling is not None:
+            dangling_weights = (alpha / dangling.reduce().value * dangling).new(
+                name="dangling_weights"
+            )
+        elif personalization is None:
+            # Fast case (and common case); is iso-valued
+            dangling_mask(mask=dangling_mask.S) << alpha * p
+        else:
+            dangling_weights = (alpha * p).new(name="dangling_weights")
+
+    # Fold constant into p
+    p *= 1 - alpha
+
+    # Power iteration: make up to max_iter iterations
+    xnew = Vector.new(float, N, name="xnew")
+    w = Vector.new(float, N, name="w")
+    for _ in range(max_iter):
+        # xnew << alpha * ((x * S) @ A + "dangling_weights") + (1 - alpha) * p
+        xnew << p
+        if is_dangling:
+            if dangling is None and personalization is None:
+                # Fast case: add a scalar; xnew is still iso-valued (b/c p is also scalar)
+                xnew += x @ dangling_mask
+            else:
+                # Add a vector
+                xnew += plus_first(x @ dangling_mask) * dangling_weights
+        w << x * S
+        xnew += semiring(w @ A)  # plus_first if A.ss.is_iso else plus_times
+
+        # Check convergence, l1 norm: err = sum(abs(x - xnew))
+        x << binary.minus(x & xnew)
+        x << unary.abs(x)
+        err = x.reduce().value
+        if err < N * tol:
+            xnew.name = name
+            return xnew
+        x, xnew = xnew, x
+    raise nx.PowerIterationFailedConvergence(max_iter)
 
 
 def pagerank(
@@ -21,9 +116,10 @@ def pagerank(
     if N == 0:
         return {}
     nodelist = list(G)
-    # grblas io functions should be able to do this
+    # grblas io functions should be able to do this more conveniently, and it would
+    # be best if it could determine whether the output matrix is iso-valued or not.
     A = nx.to_scipy_sparse_array(G, nodelist=nodelist, weight=weight, dtype=float)
-    A = gb.Matrix.ss.import_csr(
+    A = Matrix.ss.import_csr(
         nrows=A.shape[0],
         ncols=A.shape[1],
         indptr=A.indptr,
@@ -33,71 +129,38 @@ def pagerank(
         take_ownership=True,
         name="A",
     )
-    # If we know A is purely structural (comprised of only 1 values),
-    # we can structure the calculation to use the plus_first semiring).
-    S = A.reduce_rowwise().new(name="S")
-    # Fold alpha constant into A
-    A << any_times((alpha / S).diag() @ A)
-
-    # Initial vector
-    if nstart is None:
-        x = gb.Vector.new(float, N)
-        x[:] = 1.0 / N
-    else:
-        x = np.array([nstart.get(n, 0) for n in nodelist], dtype=float)
-        x /= x.sum()
-        x = gb.Vector.ss.import_full(x, take_ownership=True, name="x")
-
-    # Personalization vector
-    if personalization is None:
-        p = 1.0 / N
-    else:
-        p = np.array([personalization.get(n, 0) for n in nodelist], dtype=float)
-        if p.sum() == 0:
-            raise ZeroDivisionError()
-        p /= p.sum()
-        p = gb.Vector.ss.import_full(p, take_ownership=True, name="p")
-
-    # Dangling nodes
-    is_dangling = S.nvals < N
-    if is_dangling:
-        dangling_mask = gb.Vector.new(float, N, name="dangling_mask")
-        dangling_mask(mask=~S.S) << 1.0
-        # Fold alpha constant into dangling_weights (or dangling_mask)
-        if dangling is not None:
-            # Convert the dangling dictionary into an array in nodelist order
-            weights = np.array([dangling.get(n, 0) for n in nodelist], dtype=float)
-            weights *= alpha / weights.sum()
-            dangling_weights = gb.Vector.ss.import_full(
-                weights, take_ownership=True, name="dangling_weights"
+    x = p = dangling_weights = None
+    # Initial vector (we'll normalize later)
+    if nstart is not None:
+        x = Vector.ss.import_full(
+            np.array([nstart.get(n, 0) for n in nodelist], dtype=float),
+            take_ownership=True,
+            name="nstart",
+        )
+    # Personalization vector (we'll normalize later)
+    if personalization is not None:
+        p = Vector.ss.import_full(
+            np.array([personalization.get(n, 0) for n in nodelist], dtype=float),
+            take_ownership=True,
+            name="personalization",
+        )
+    # Dangling nodes (we'll normalize later)
+    row_degrees = A.reduce_rowwise().new(name="row_degrees")
+    if dangling is not None:
+        if row_degrees.nvals < N:  # is_dangling
+            dangling_weights = Vector.ss.import_full(
+                np.array([dangling.get(n, 0) for n in nodelist], dtype=float),
+                take_ownership=True,
+                name="dangling",
             )
-        elif personalization is None:
-            # Fast case (and common case)
-            dangling_mask(mask=dangling_mask.S) << alpha * p
-        else:
-            dangling_weights = (alpha * p).new(name="dangling_weights")
-
-    # Fold decay constant into p
-    p *= 1 - alpha
-    xnew = gb.Vector.new(float, N, name="xnew")
-    # Power iteration: make up to max_iter iterations
-    for _ in range(max_iter):
-        # xnew << alpha * (x @ A + "dangling_weights") + (1 - alpha) * p
-        xnew << p
-        if is_dangling:
-            if dangling is None and personalization is None:
-                # Fast case: add a scalar; xnew is still iso-valued (b/c p is also scalar)
-                xnew += x @ dangling_mask
-            else:
-                # Add a vector
-                xnew += plus_first(x @ dangling_mask) * dangling_weights
-        # We use plus_times semiring here b/c A may be weighted or from a multigraph.
-        # If we're clever, we could figure out when we can use plus_first semiring.
-        xnew += x @ A
-
-        # Check convergence, l1 norm
-        err = gb.binary.minus(x & xnew).apply(gb.unary.abs).reduce().value
-        if err < N * tol:
-            return dict(zip(nodelist, xnew.to_values()[1]))
-        x, xnew = xnew, x
-    raise nx.PowerIterationFailedConvergence(max_iter)
+    result = pagerank_core(
+        A,
+        alpha=alpha,
+        personalization=p,
+        max_iter=max_iter,
+        tol=tol,
+        nstart=x,
+        dangling=dangling_weights,
+        row_degrees=row_degrees,
+    )
+    return dict(zip(nodelist, result.to_values()[1]))
