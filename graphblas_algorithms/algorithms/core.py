54f46fae908e299ccce9efe7ea6c7cb66f3bf0c5
Jim Kitchen <jim22k@gmail.com>
2022-08-02 11:51:20 -0500

    Split k_truss into core and wrapper versions

--- graphblas_algorithms/algorithms/core.py
+++ graphblas_algorithms/algorithms/core.py
@@ -5,14 +5,17 @@ from graphblas_algorithms.classes.graph import to_undirected_graph, Graph
 from graphblas_algorithms.utils import get_all, not_implemented_for
 
 
-@not_implemented_for("directed")
-def k_truss(G, k):
-    graph = to_undirected_graph(G, dtype=bool)
-    id_to_key = graph.id_to_key
-    S = graph.get_property("offdiag")
+def k_truss_core(G, k):
+    id_to_key = G.id_to_key
+    # Ignore self-edges
+    S = G.get_property("offdiag")
 
-    if k >= 3:
-        # Remove edges no in k-truss
+    if k < 3:
+        # Most implementations consider k < 3 invalid,
+        # but networkx leaves the graph unchanged
+        C = S
+    else:
+        # Remove edges not in k-truss
         nvals_last = S.nvals
         C = Matrix("int32", S.nrows, S.ncols)
         while True:
@@ -22,10 +25,6 @@ def k_truss(G, k):
                 break
             nvals_last = C.nvals
             S = C
-    else:
-        # Most implementations consider this invalid, but
-        # networkx tests leave the graph unchanged for k < 3
-        C = S
 
     # Remove isolate nodes
     idx, _ = C.reduce_rowwise(agg.count).to_values()
@@ -34,7 +33,14 @@ def k_truss(G, k):
     # Convert back to networkx graph with correct node ids
     indexes = [id_to_key[i] for i in idx]
     key_to_id = dict(zip(indexes, range(len(idx))))
-    return Graph.from_graphblas(Ktruss, key_to_id=key_to_id).to_networkx()
+    return Graph.from_graphblas(Ktruss, key_to_id=key_to_id)
+
+
+@not_implemented_for("directed")
+def k_truss(G, k):
+    G = to_undirected_graph(G, dtype=bool)
+    result = k_truss_core(G, k)
+    return result.to_networkx()
 
 
 __all__ = get_all(__name__)
